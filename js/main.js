6afd2f4330a2490811bdc6b62003f163dfc91b57
Ethen1264 <jxorhks86909763@gmail.com>
2023-07-14 12:32:44 +0900

    update stabucks

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const serchEl = document.querySelector(".search");
+const serchInputEl = serchEl.querySelector("input");
+
+serchEl.addEventListener("click",function(){
+  serchInputEl.focus();
+});
+
+serchInputEl.addEventListener("focus", function(){
+  serchEl.classList.add("focused");
+  serchInputEl.setAttribute("placeholder", "통합검색")
+});
+
+serchInputEl.addEventListener("blur", function(){
+  serchEl.classList.remove("focused");
+  serchInputEl.setAttribute("placeholder", "")
+});
